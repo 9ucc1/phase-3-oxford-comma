f9d6a48582115e33eabd555295246d6760834ad5
Gucci Fan <gucc@umich.edu>
2023-01-26 12:42:09 -0500

    commit tree 50a6dd318178d770b02f494c96144e999fd89a0a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array[-1] = "and #{array[-1]}"
+        array2 = array.join(", ")
+        array2
+    end
 end
+#binding.pry
